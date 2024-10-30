ce1751114e260dd8a14040e69f0d0a438ef0e353
Hapiii-dev <hapihambi@gmail.com>
2024-10-30 17:10:27 +0800

    ai.js

--- ws3/commands/ai.js
+++ ws3/commands/ai.js
@@ -21,7 +21,7 @@ Example: ${api.prefix + name} what is love?`);
     throw new Error();
     send(`${gpt.data.result}
 
-🤖 WieAI by Neth Aceberos`);
+🤖 HAPi Ai by Jervie Macapagal`);
     } catch(err){
       send(err.message || err);
       return;
